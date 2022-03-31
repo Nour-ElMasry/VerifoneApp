767daac81cd4447a3fb5c1eef1e8c0578f330758
Elafifi Nour-Eldin <59283990+Nour-ElMasry@users.noreply.github.com>
2022-03-31 22:54:57 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+const fetch = require('node-fetch');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(__dirname + '/public'));
+
+let api = "http://private-32dcc-products72.apiary-mock.com/product";
+
+mongoose.connect("mongodb+srv://admin-nour:admin@cluster0.nhj1z.mongodb.net/Products?retryWrites=true&w=majority");
+
+const productSchema = new mongoose.Schema({
+    _id: Number,
+    name: String,
+    price: Number,
+    description: String,
+    addedToCart: Boolean
+});
+
+const Product = mongoose.model('product', productSchema);
+
+Product.find({}, (err, data) => {
+    if (data.length == 0) {
+        fetch(api, { method: "Get" })
+            .then(res => res.json())
+            .then((json) => {
+                json.forEach(async p => {
+                    let product = new Product({
+                        _id: p.id,
+                        name: p.name,
+                        price: p.price,
+                        description: p.description,
+                        addedToCart: false
+                    });
+                    await product.save();
+                });
+            });
+    }
+});
+
+// var currency = "eur";
+
+app.get("/", (req, res) => {
+    // currency = req.params.currency;
+    Product.find({ addedToCart: false }, (err, products) => {
+        if (err) {
+            console.log(err)
+        } else {
+            Product.find({ addedToCart: true }, (err, cartProd) => err ? console.log(err) : res.render("home", { products: products, cartProducts: cartProd })).sort({ _id: 1 });
+        }
+    }).sort({ _id: 1 });
+});
+
+// app.get("/:currency?/getSelected", (req, res) => {
+//     res.send(currency);
+// })
+
+app.post("/addProdToCart", (req, res) => {
+    Product.updateOne({ _id: req.body.cartProd }, { addedToCart: true }, (err) => err ? console.log(err) : res.redirect("/" + currency));
+});
+
+app.post("/delete", (req, res) => {
+    Product.updateOne({ _id: req.body.removeProd }, { addedToCart: false }, (err) => err ? console.log(err) : res.redirect("/" + currency))
+});
+
+app.post("/reset", (req, res) => {
+    Product.updateMany({ addedToCart: true }, { addedToCart: false }, (err) => err ? console.log(err) : res.redirect("/" + currency));
+});
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server started");
+});
---
+++ public/js/index.js
@@ -0,0 +1,108 @@
+const selectedCurrency = document.querySelector("#currency-select");
+const currency = document.querySelectorAll(".currency");
+const price = document.querySelectorAll(".price");
+let eurPrices = [];
+
+price.forEach(p => eurPrices.push(parseFloat(p.innerHTML)));
+
+var selected;
+
+function ajaxTemplate(url, type, datatype) {
+    return new Promise(function(resolve, reject) {
+        $.ajax({
+            url: url,
+            type: type,
+            dataType: datatype,
+            success: (txt) => {
+                resolve(txt);
+            },
+            error: (err) => {
+                reject(err);
+            }
+        });
+    });
+}
+
+
+// ajaxTemplate(window.location.href + "/getSelected", "GET", "text").then(function(data) {
+//     selectedCurrency.value = data;
+// }).catch(function(err) {
+//     console.log(err)
+// });
+
+
+function CurrencyConvertor() {
+    var fromEURtoUSD, fromEURtoGBP;
+    apiUrl = "http://data.fixer.io/api/latest?access_key=ba1f541744856e9f930fb8121c7be2c0";
+
+    ajaxTemplate(apiUrl, "GET", "jsonp").then(function(data) {
+        fromEURtoUSD = data.rates.USD;
+        fromEURtoGBP = data.rates.GBP;
+    }).catch(function(err) {
+        console.log(err)
+    });
+
+    selectedCurrency.onchange = () => {
+        var selectedCur = selectedCurrency.value;
+        let newPrice;
+        for (let i = 0; i < price.length; i++) {
+            switch (selectedCur) {
+                case "eur":
+                    currency[i].innerHTML = "€";
+                    price[i].innerHTML = eurPrices[i];
+                    break;
+                case "usd":
+                    currency[i].innerHTML = "$";
+                    newPrice = eurPrices[i] * fromEURtoUSD
+                    price[i].innerHTML = newPrice.toFixed(2);
+                    break;
+                case "gbp":
+                    currency[i].innerHTML = "£";
+                    newPrice = eurPrices[i] * fromEURtoGBP
+                    price[i].innerHTML = newPrice.toFixed(2);
+                    break;
+                default:
+                    console.log("No such currency");
+            }
+        }
+        calcTotal();
+        ProductQuantities();
+    }
+}
+
+const quantity = document.querySelectorAll(".quantity");
+const value = document.querySelectorAll("#quantityPrice");
+const cartTotal = document.querySelector(".cartTotal");
+
+function ProductQuantities() {
+    let initialPrices = [];
+    value.forEach(v => initialPrices.push(parseFloat(v.innerHTML)))
+    for (let i = 0; i < quantity.length; i++) {
+        if (quantity[i] > 1) {
+            let q = parseFloat(quantity[i].value);
+            let result = (initialPrices[i] * q).toFixed(2);
+            value[i].innerHTML = result;
+            calcTotal();
+        }
+        quantity[i].onchange = () => {
+            let q = parseFloat(quantity[i].value);
+            let result = (initialPrices[i] * q).toFixed(2);
+            value[i].innerHTML = result;
+            calcTotal();
+        }
+    }
+}
+
+function calcTotal() {
+    let total = 0;
+    value.forEach(v => {
+        total = (total + parseFloat(v.innerHTML));
+    })
+    cartTotal.innerHTML = total.toFixed(2);
+}
+
+CurrencyConvertor();
+if (quantity.length != 0) {
+    ProductQuantities();
+    calcTotal();
+}
